35af75bf0b536569341f2b642b37a46e4db2171c
KingPinGi <ainatobi83@gmail.com>
2022-10-20 12:32:01 +0100

    more commit

--- redux/src/store/index.js
+++ redux/src/store/index.js
@@ -1,39 +1,12 @@
-import { createStore } from "redux";
-import { createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
-const initialState = { counter: 0, showCounter: true };
-const counterReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
+import counterReducer from "./counter";
+import authReducer from "./auth";
 
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
+const store = configureStore({
+  reducer: { counter: counterReducer, auth: authReducer },
+});
 
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
 
-  if (action.type === "toggle") {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter,
-    };
-  }
-
-  return state;
-};
-
-const store = createStore(counterReducer);
 
 export default store;
